393e16621027f036d14b3f42ed2efeb6b7686ad4
John Barker <jdabarker@gmail.com>
2021-06-04 13:04:20 +0800

    implemented categories GET routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,28 @@ const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({ include: Product });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: Product,
+    });
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 router.post("/", (req, res) => {
